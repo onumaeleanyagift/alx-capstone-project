1aea8ccf9d75d44482fc270654a14019fb58ff18
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-04-05 16:24:35 +0100

    add routes for JobCategory and FillProfile components

--- job-finder/src/App.jsx
+++ job-finder/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./index.css";
 import WelcomePage from "./pages/WelcomePage";
 import SignUp from "./components/SignUp";
 import SignIn from "./components/SignIn";
-import "./index.css";
+import JobCategory from "./pages/JobCategory";
+import FillProfile from "./pages/FillProfile";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/JobCategory" element={<JobCategory />} />
+        <Route path="/FillProfile" element={<FillProfile />} />
       </Routes>
     </Router>
   );
