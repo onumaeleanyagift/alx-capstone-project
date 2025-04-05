61eadc5a7b5aaabb1a0281b661f9ed488edcf559
onumaeleanyagift <onumaeleanyagift@gmail.com>
2025-04-05 05:26:32 +0100

    Add SignIn component to App.jsx

--- job-finder/src/App.jsx
+++ job-finder/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
 import "./index.css";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/SignUp" element={<SignUp />} />
+        <Route path="/SignIn" element={<SignIn />} />
       </Routes>
     </Router>
   );
